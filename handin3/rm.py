a299dfb67769e74d81ce113a1ff7146b8c88fcc8
Sepehr Tayari <sepehr@Sepehrs-MacBook-Pro.local>
2020-05-20 12:23:26 +0200

    matrix with array type. Doesnt work

--- handin3/rm.py
+++ handin3/rm.py
@@ -16,12 +16,16 @@ class RM:
     def _g(self, r, m):
         print("recThis is r{} and this is m{}".format(r,m))
         if r == 0 :
-            return self._ones(pow(2,m))
+            #return self._ones(pow(2,m))
+            return np.ones(pow(2,m))
         elif r == m:
-            return  self._identity(pow(2,m))
+            #return  self._identity(pow(2,m))
+            return np.identity(pow(2,m))
         else:
-            return [self._g(r,m-1), self._g(r,m-1),
-                            [0,         self._g(r-1,m-1)]]
+            return np.array([
+                [self._g(r,m-1),        self._g(r,m-1)],
+                [self._zeros(pow(2,m)), self._g(r-1,m-1)]
+                ])
 
     
     def _identity(self, n):
@@ -35,10 +39,20 @@ class RM:
         for _ in range(m): 
             arr.append(1)
         return arr
+    
+    def _zeros(self, m):
+        arr = []
+        for _ in range(m): 
+            arr.append(0)
+        return arr
 
 
 if __name__ == '__main__':
     reed_muller = RM(1,3)
-    print(str(reed_muller.g(1,3)))
+    matrix= reed_muller.g(1,3)
+    print(matrix)
+    #for row in matrix:
+    #    print(row)
+    #print(str(reed_muller.g(1,3)))
     
 
